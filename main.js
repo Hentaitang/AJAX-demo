0cd60318985c41c47787a78b90bbf899d07cf553
Hentaitang <mz374427166@gmail.com>
2018-08-01 14:55:13 +0800

    第二次修改

--- main.js
+++ main.js
@@ -1,22 +1,118 @@
-myButton.addEventListener('click', (e)=>{
+window.jQuery = function(nodeOrSelector){
+    let nodes = {}
+    nodes.addClass = function(){}
+    nodes.html = function(){}
+    return nodes
+}
+
+window.jQuery.ajax = function({method, body, headers, successFn, failFn}){
+    let url
+    if(arguments.length === 1){
+        url = options.url
+    }else if(arguments.length === 2){
+        url = arguments[0]
+        options = arguments[1]
+    }
+    //let method = options.method
+    //let body = options.body
+    //let headers = options.headers
+    //let successFn = options.successFn
+    //let failFn = options.failFn
+
+
+    // ES6语法  析构赋值
+    // let {method, body, headers, successFn, failFn} = options
+
     let request = new XMLHttpRequest() //声明一个XMLHttpRequest对象
+    request.open(method, url) // 配置request
+    for(key in headers){
+        let value = headers[key]
+        request.setRequestHeader(key, value)
+    }
     request.onreadystatechange = ()=>{
         if(request.readyState === 4){
-            console.log('请求响应都完毕了')
-            console.log(request.status)
             if(request.status >= 200 && request.status < 300){
-                console.log('请求成功')
-                console.log(request.responseText)
-                let string = request.responseText
-                // 把符合 JSON 语法的字符串
-                // 转换成 JS 对应的值
-                let object = window.JSON.parse(string)
-                // JSON.parse 是浏览器提供的
+                successFn.call(undefined, request.responseText)
             }else if(request.status >= 400){
-                console.log('说明请求失败')
+                failFn.call(undefined, request)
             }
         }
     }
-    request.open('GET', 'http://jack.com:8002/xxx') // 配置request
-    request.send() //发送
+    request.send(body) //发送
+}
+
+window.$ = window.jQuery
+
+function f1(responseText){}
+function f2(responseText){}
+
+myButton.addEventListener('click', (e)=>{
+    window.jQuery.ajax(
+        {
+            url: '/frank',
+            method: 'get',
+            success: (x)=>{
+                console.log(x)
+                f1.call(undefined, x)
+                f2.call(undefined, x)
+            },
+            error: (x)=>{
+                console.log(x)
+                console.log(x.status)
+            }
+        }
+    )
+
+    //let obj = {
+    //    url: '/xxx',
+    //    method: 'get',
+    //    successFn: ()=>{},
+    //    failFn: ()=>{}
+    //}
+    
+    //window.jQuery.ajax(
+    //    {
+    //        url: '/frank',
+    //        method: 'get',
+    //        headers: {
+    //            'Content-Type': 'application/x-www-form-urlencode',
+    //            'frank' : '18' 
+    //        },
+    //        successFn: (x)=>{
+    //            console.log(x)
+    //            f1.call(undefined, x)
+    //            f2.call(undefined, x)
+    //        },
+    //        failFn: (x)=>{
+    //           console.log(x)
+    //            console.log(x.status)
+    //        }
+    //    }
+//    )
+
+    //let request = new XMLHttpRequest() //声明一个XMLHttpRequest对象
+    //request.onreadystatechange = ()=>{
+    //    if(request.readyState === 4){
+    //        console.log('请求响应都完毕了')
+    //        console.log(request.status)
+    //        console.log(request.statusText)
+    //        if(request.status >= 200 && request.status < 300){
+    //            console.log('请求成功')
+    //            console.log(request.getAllResponseHeaders())
+    //            console.log(request.getResponseHeader('Content-Type'))
+    //            console.log(request.responseText)  
+    //            let string = request.responseText
+    //            // 把符合 JSON 语法的字符串
+    //            // 转换成 JS 对应的值
+    //            let object = window.JSON.parse(string)
+    //            // JSON.parse 是浏览器提供的
+    //        }else if(request.status >= 400){
+    //            console.log('说明请求失败')
+    //        }
+    //    }
+    //}
+    //request.open('POST', '/xxx') // 配置request
+    //request.setRequestHeader('frank', '18')
+    //request.setRequestHeader('Content-Type', 'x-www-form-urlencoded')
+    //request.send('我偏要设置第四部分') //发送
 })
